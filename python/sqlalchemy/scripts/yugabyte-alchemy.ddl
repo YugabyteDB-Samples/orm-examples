7530bfd5c48a9217e4a1310fab5bb032ae913447
George Nowakowski <gnowakow@amazon.com>
2019-05-02 20:40:28 -0400

    Implemented changes suggested in PR - logging, clean up

--- python/sqlalchemy/scripts/yugabyte-alchemy.ddl
+++ python/sqlalchemy/scripts/yugabyte-alchemy.ddl
@@ -1,33 +1,31 @@
 
-create database alchemy_orm_sample;
+create database yugabyte_alchemy;
 
-\c alchemy_orm_sample
+\c yugabyte_alchemy
 
 create table users (
-	user_id 		serial primary key,
-	first_name		varchar(40) not null,
-	last_name		varchar(40) not null,
-	email			varchar(40)
+    user_id             serial primary key,
+    first_name          varchar(40) not null,
+    last_name           varchar(40) not null,
+    email               varchar(40)
 );
 
 create table products (
-	product_id		serial primary key,
-	product_name	varchar(50),
-	description		varchar(500),
-	price  			numeric(8,2)
+    product_id          serial primary key,
+    product_name        varchar(50),
+    description         varchar(500),
+    price               numeric(8,2)
 );
 
 create table orders (
-	order_id		serial primary key,
-	user_id			numeric(8, 2),
-	order_total     numeric(8, 2)
+    order_id            serial primary key,
+    user_id             numeric(8, 2),
+    order_total         numeric(8, 2)
 );
 
 create table order_lines (
-	line_id			serial primary key,
-	order_id		bigint,
-	product_id      integer,
-	units           bigint
+    line_id             serial primary key,
+    order_id            bigint,
+    product_id          integer,
+    units               bigint
 );
-
-\c alchemy_orm_sample
---
+++ python/sqlalchemy/src/rest-service.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import logging
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+from io import BytesIO
+import jsonpickle
+from yugabyte_alchemy_model import User, Order, OrderLine, Product
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
+
+logging.getLogger('sqlalchemy.engine.base.Engine').setLevel(logging.WARNING)
+
+listen_port = 8000
+db_user = 'postgres'
+db_password = 'pwd'
+database = 'yugabyte_alchemy'
+db_host = 'localhost'
+db_port = 5433
+
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://%s:%s@%s:%s/%s' % 
+                       (db_user, db_password, db_host, db_port, database), echo=True)
+Session = sessionmaker(bind=engine)
+
+
+def add_object(session, to_add):
+    session.add(to_add)
+    logging.debug('Added: %s' % str(to_add) )
+
+
+def add_objects(session, object_list):
+    session.add_all(object_list)
+    logging.debug('Added (list): ')
+    [logging.debug(str(obj)) for obj in object_list]
+
+class DataAccessUtil:
+
+    def get_users(self):
+        session = Session()
+
+        try:
+            users = session.query(User).all()
+            users_json = {
+                'users': [
+                    user.to_json() for user in users
+                ]
+            }
+            session.commit()
+            return users_json
+        except Exception as e:     
+            logging.debug('*** Exception in get_users: %s' % e)
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def list_orders_for_user(self, rq_user_id):
+        session = Session()
+
+        try:
+            user = session.query(User).get(rq_user_id)
+
+            if user is None:
+                logging.debug('Unable to find user with id = %s' % rq_user_id)
+                # TODO: Raise an error
+
+            user_json = user.to_json()
+
+            user_json['orders'] = [
+                order.to_json() for order in user.orders
+            ]
+            return user_json
+        except Exception as e:     
+            logging.debug('*** Exception in get_users: %s' % e)
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def create_order(self, rq_user_id, products):
+        session = Session()
+
+        try:
+            user = session.query(User).get(rq_user_id)
+
+            if user is None:
+                raise Exception('Unable to find user with id = %s' % rq_user_id)
+
+            order_line_list = []
+            user_order = Order(user_id=rq_user_id, order_total=0)
+
+            logging.debug('Processing %s products' % len(products))
+            for prod_def in products:
+                db_product = session.query(Product).get(prod_def['productId'])
+                if db_product is None:
+                    raise Exception('Product not found with ProductID: %s' % prod_def['productId'])
+                order_line = OrderLine(product=db_product, order=user_order, units=prod_def['units'])
+                order_line_list.append(order_line)
+                user_order.order_total = user_order.order_total + db_product.price
+
+            session.add(user_order)
+            session.add_all(order_line_list)
+            session.commit()
+
+            the_order = session.query(Order).get(user_order.order_id)
+
+            return the_order.to_json()
+
+        except Exception as e:     
+            logging.debug('*** Exception in get_users: %s' % e)
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+    def create_product(self, to_add):
+        session = Session()
+
+        try:
+            product = Product(product_name=to_add['productName'], description=to_add['description'], price=to_add['price'])
+            self.add_object(product)
+            session.commit()
+
+            return product.to_json()
+        except Exception as e:     
+            logging.debug('*** Exception in get_users: %s' % e)
+            session.rollback()
+            raise
+        finally:
+            session.close()
+ 
+    def add_object(self, to_add):
+        session = Session()
+
+        try:
+            session.add(to_add)
+            session.commit()
+            return to_add.to_json()
+        except Exception as e:
+            logging.debug('** Exception while trying to add object: %s Error: %e' % (str(to_add), e))
+            raise
+        finally:
+            session.close()
+
+    def list_products(self):
+        session = Session()
+
+        try:
+            products = session.query(Product).all()
+            product_json = {
+                'products': [
+                    product.to_json() for product in products
+                ]
+            }
+            session.commit()
+            return product_json
+        except Exception as e:     
+            logging.debug('*** Exception in get_users: %s' % e)
+            session.rollback()
+            raise
+        finally:
+            session.close()
+
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'json')
+        self.end_headers()
+
+    def _return_error(self):
+        self.send_response(400)
+
+    def do_GET(self):
+        logging.debug('do_Get called with path: %s' % self.path)
+        dao = DataAccessUtil()
+
+        if self.path == '/users':
+            self.handle_list_users(dao)
+
+        if self.path == '/products':
+            self.handle_list_products(dao)
+
+        if self.path == '/list-products':
+            self.handle_list_all_products(dao)
+
+    def do_POST(self):
+
+        logging.debug('>> do_POST')
+        content_length = int(self.headers['Content-Length'])
+        body = json.loads(self.rfile.read(content_length))
+
+        logging.debug('body: ' % body)
+
+        dao = DataAccessUtil()
+
+        logging.debug('doPost called with url: %s and data: %s' % (self.path, body))
+
+        try:
+            if self.path == '/users':
+                self.handle_create_user(dao, body)
+
+            if self.path == '/orders':
+                self.handle_create_order(dao, body)
+
+            if self.path == '/list-orders':
+                self.handle_list_orders(dao, body)
+
+            if self.path == '/products':
+                self.handle_create_product(dao, body)
+
+            if self.path == '/list-products':
+                self.handle_list_all_products(dao)
+        except: 
+            self._return_error()
+
+    # =======================================================
+    ## Handlers below
+    # =======================================================
+
+    def handle_create_user(self, dao, body):
+        self.handle_write_output(
+            dao.add_object(User(first_name=body['firstName'], last_name=body['lastName'], email=body['email'])))
+
+    def handle_list_users(self, dao):
+        self.handle_write_output(dao.get_users())
+
+    def handle_list_all_products(self, dao):
+        self.handle_write_output(
+            dao.list_products()
+        )
+
+    def handle_create_product(self, dao, body):
+        self.handle_write_output(
+            dao.create_product(body)
+        )
+
+    def handle_list_orders(self, dao, body):
+        self.handle_write_output(
+            dao.list_orders_for_user(body['userId'])
+        )
+
+    def handle_list_products(self, dao, body):
+        self.handle_write_output(
+            dao.list_products()
+        )
+        
+
+    def handle_write_output(self, results_json):
+        self._set_headers()
+        response = BytesIO()
+        response.write(jsonpickle.encode(results_json).encode())
+        self.wfile.write(response.getvalue())
+
+    def handle_create_order(self, dao, body):
+        if 'userId' not in body:
+            logging.debug('UserId required!')
+            # TODO: Return an error here!
+
+        self.handle_write_output(dao.create_order(body['userId'], body['products']))
+
+# ### HTTP Server
+httpd = HTTPServer(('localhost', listen_port), SimpleHTTPRequestHandler)
+logging.debug('Listening on port %s' % listen_port)
+httpd.serve_forever()
+
