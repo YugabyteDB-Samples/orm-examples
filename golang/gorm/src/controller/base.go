2501eed28a6be3957310a2b5046df3eba53de9bc
Azeem Shahzad <azeem.shahzad@gmail.com>
2019-05-08 09:55:48 -0400

    Changes as per review by rajukumaryb

--- golang/gorm/src/controller/base.go
+++ golang/gorm/src/controller/base.go
@@ -14,12 +14,11 @@ func init() {
 	dbUri := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable password=%s", config.Config.Host, config.Config.DbPort, config.Config.Username, config.Config.Dbname, config.Config.Password)
 	fmt.Println("Connecting: " + dbUri)
 
-	conn, err := gorm.Open("postgres", dbUri)
+	db, err := gorm.Open("postgres", dbUri)
 	if err != nil {
 		panic(err)
 	}
 
-	db = conn
 	db.Debug().AutoMigrate(&model.User{}, &model.Product{}, &model.Order{}, &model.OrderLine{})
 
 	db.Model(&model.OrderLine{}).AddForeignKey("order_id", "orders(order_id)", "CASCADE", "CASCADE")
