af2c016629dd4c3dbbbaa313ba8094dabf38caa9
Azeem Shahzad <azeem.shahzad@gmail.com>
2019-05-11 07:04:14 -0400

    changed declartion statement to assignment statement

--- golang/gorm/src/controller/base.go
+++ golang/gorm/src/controller/base.go
@@ -8,22 +8,23 @@ import (
 	"model"
 )
 
-var db *gorm.DB
+var dbConn *gorm.DB
 
 func init() {
 	dbUri := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable password=%s", config.Config.Host, config.Config.DbPort, config.Config.Username, config.Config.Dbname, config.Config.Password)
 	fmt.Println("Connecting: " + dbUri)
 
-	db, err := gorm.Open("postgres", dbUri)
+	var err error 
+	dbConn, err = gorm.Open("postgres", dbUri)
 	if err != nil {
 		panic(err)
 	}
 
-	db.Debug().AutoMigrate(&model.User{}, &model.Product{}, &model.Order{}, &model.OrderLine{})
+	dbConn.Debug().AutoMigrate(&model.User{}, &model.Product{}, &model.Order{}, &model.OrderLine{})
 
-	db.Model(&model.OrderLine{}).AddForeignKey("order_id", "orders(order_id)", "CASCADE", "CASCADE")
-	db.Model(&model.OrderLine{}).AddForeignKey("product_id", "products(product_id)", "CASCADE", "CASCADE")
-	db.Model(&model.Order{}).AddForeignKey("user_id", "users(user_id)", "CASCADE", "CASCADE")
+	dbConn.Model(&model.OrderLine{}).AddForeignKey("order_id", "orders(order_id)", "CASCADE", "CASCADE")
+	dbConn.Model(&model.OrderLine{}).AddForeignKey("product_id", "products(product_id)", "CASCADE", "CASCADE")
+	dbConn.Model(&model.Order{}).AddForeignKey("user_id", "users(user_id)", "CASCADE", "CASCADE")
 }
 
-func GetDB() *gorm.DB { return db }
+func GetDB() *gorm.DB { return dbConn }
