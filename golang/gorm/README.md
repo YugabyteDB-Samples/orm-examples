736b7919c002761eb05f8c79da5f1bb67d28beaf
azeemshahzad <48369329+azeemshahzad@users.noreply.github.com>
2019-05-13 18:48:16 -0400

    golang/gorm example implementation (#5)
    * Change Product:product_id and Users:user_id from UUID to Serial
    
    * Support added for Sale-Units and array-style Product listing in the Order
    
    * initial code for golang gorm
    
    * deleted eclipse related files
    
    * initial code for golang gorm
    
    * log error if listen method failed
    
    * compile, run and deployment instructions
    
    * environent setup instructions
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * Changes as per review by rajukumaryb
    
    * removed un-necessary print statement
    
    * changed declartion statement to assignment statement
    
    * fixed typos in struct declaration
    
    * added 'gorm' library dependency

---
+++ golang/gorm/README.md
@@ -0,0 +1,29 @@
+# Compile, Deploy and Run
+Install required components
+```
+go get github.com/jinzhu/gorm
+go get github.com/google/uuid
+go get github.com/gorilla/mux
+```
+Append source code location to GOPATH environment variable
+```
+export GOPATH=$GOPATH: /orm-examples/golang/gorm
+```
+Configure environment in /orm-examples/golang/gorm/src/config/config.json
+```
+{
+	"platform" : "postgres",
+	"host" : "localhost",
+	"dbport" : 5433,
+	"username" : "postgres",
+	"password" : "",
+	"dbname" : "gormdemo",
+	"sslmode" : "disable",
+	"bindport" : 8080
+}
+```
+Build and run using shell script
+```
+./build-and-run.sh
+```
+
---
+++ golang/gorm/src/controller/base.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"config"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"model"
+)
+
+var dbConn *gorm.DB
+
+func init() {
+	dbUri := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable password=%s", config.Config.Host, config.Config.DbPort, config.Config.Username, config.Config.Dbname, config.Config.Password)
+	fmt.Println("Connecting: " + dbUri)
+
+	var err error 
+	dbConn, err = gorm.Open("postgres", dbUri)
+	if err != nil {
+		panic(err)
+	}
+
+	dbConn.Debug().AutoMigrate(&model.User{}, &model.Product{}, &model.Order{}, &model.OrderLine{})
+
+	dbConn.Model(&model.OrderLine{}).AddForeignKey("order_id", "orders(order_id)", "CASCADE", "CASCADE")
+	dbConn.Model(&model.OrderLine{}).AddForeignKey("product_id", "products(product_id)", "CASCADE", "CASCADE")
+	dbConn.Model(&model.Order{}).AddForeignKey("user_id", "users(user_id)", "CASCADE", "CASCADE")
+}
+
+func GetDB() *gorm.DB { return dbConn }
