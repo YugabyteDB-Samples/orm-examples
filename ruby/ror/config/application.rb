4499016b4b4ef07404fdec0d363a4a19a4242096
wasif <wasifshehzad063@gmail.com>
2019-05-24 03:47:17 +0500

    add env file

--- ruby/ror/config/application.rb
+++ ruby/ror/config/application.rb
@@ -9,18 +9,16 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
-
 Bundler.require(*Rails.groups)
 
 module Ror
   class Application < Rails::Application
     config.load_defaults 5.2
-
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
-
     config.api_only = true
   end
 end
