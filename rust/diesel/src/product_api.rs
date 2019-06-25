443663303237626253469c49a4348af9f2da6446
svranesevic <svranesevic@users.noreply.github.com>
2019-06-25 22:33:46 +0200

    Fix response DTOs

--- rust/diesel/src/product_api.rs
+++ rust/diesel/src/product_api.rs
@@ -1,16 +1,45 @@
+use bigdecimal::BigDecimal;
 use rocket_contrib::json::Json;
+use serde::Serialize;
 
 use crate::db;
 use crate::product::{NewProduct, Product};
 
 #[post("/products", data = "<product>", format = "json")]
-pub fn create_product(product: Json<NewProduct>, connection: db::Connection) {
-    Product::create(product.into_inner(), &connection);
+pub fn create_product(product: Json<NewProduct>, connection: db::Connection) -> Json<Product> {
+    Json(Product::create(product.into_inner(), &connection))
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProductsResponse {
+    content: Vec<ProductView>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProductView {
+    pub product_id: i32,
+    pub product_name: String,
+    pub price: BigDecimal,
 }
 
 #[get("/products")]
-pub fn read_products(connection: db::Connection) -> Json<Vec<Product>> {
-    Json(Product::read_all(&connection))
+pub fn read_products(connection: db::Connection) -> Json<ProductsResponse> {
+    let product_views = Product::read_all(&connection)
+        .iter()
+        .map(|prod| ProductView {
+            product_id: prod.product_id.clone(),
+            product_name: prod.product_name.clone(),
+            price: prod.price.clone(),
+        })
+        .collect();
+
+    let response = ProductsResponse {
+        content: product_views,
+    };
+
+    Json(response)
 }
 
 #[get("/products/<product_id>")]
