10eee8339355678bb1888ed00335061033a771e3
Azeem Shahzad <azeem.shahzad@gmail.com>
2019-07-18 11:42:13 -0400

    Python Django Example Implementation

---
+++ python/django/ybstore/urls.py
@@ -0,0 +1,29 @@
+"""ybstore URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from ybstoreapp.views import UserViewSet, ProductViewSet, OrderViewSet
+    
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'users', UserViewSet)
+router.register(r'products', ProductViewSet)
+router.register(r'orders', OrderViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls))
+]
